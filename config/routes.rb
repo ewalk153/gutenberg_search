1e8c052a43697c99b2e04f06213daca63889a792
Eric Walker <eric.walker@shopify.com>
2022-07-11 22:03:02 -0400

    Route to book_page by default

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root "book_pages#index"
   resources :books
   resources :book_pages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
